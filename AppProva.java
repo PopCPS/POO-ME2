273c074530ad25d38573209c48fbd5bfb763b3b3
Artur Coelho <102534007+PopCPS@users.noreply.github.com>
2023-12-02 19:52:11 -0300

    Update AppProva.java

--- AppProva.java
+++ AppProva.java
@@ -6,7 +6,7 @@ public class AppProva {
 
 	static QuestaoMultiplaEscolha questaoME = null;
 	static QuestaoVerdadeiroFalso questaoVF = null;
-    static QuestaoRespostaProporcional questaoRP = null;
+	static QuestaoRespostaProporcional questaoRP = null;
 
 	public static void main(String[] args) {
 		
@@ -56,246 +56,246 @@ public class AppProva {
 	
 	public static void criarQuestaoME(){
 	    
-	    scanner.nextLine();
+	scanner.nextLine();
 	    
-	    if(questaoME == null) {
-	        questaoME = new QuestaoMultiplaEscolha();
+		if(questaoME == null) {
+			questaoME = new QuestaoMultiplaEscolha();
 	        
-            System.out.print("Insira o enunciado : ");
-	        questaoME.setEnunciado(scanner.nextLine());
-	        
-	        System.out.print("Insira a primeira opção : ");
-	        questaoME.setOpcaoA(scanner.nextLine());
-	        
-	        System.out.print("Insira a segunda opção : ");
-	        questaoME.setOpcaoB(scanner.nextLine());
-	        
-	        System.out.print("Insira a terceira opção : ");
-	        questaoME.setOpcaoC(scanner.nextLine());
-	        
-            boolean firstTry = true;
-	        
-	        while(true){
-	            try{
-	                System.out.print("Insira quantos pontos vale a questão : ");
-                    if(firstTry == false){
-                        scanner.nextLine();
-	                }      
-	                firstTry = false;
-                    questaoME.setNotaPadrao(scanner.nextDouble());
-                    scanner.nextLine();
-                    break;
-	            }catch(InputMismatchException ex){
-	                System.out.println("Apenas números são válidos");
-	            }
-	        }
+			System.out.print("Insira o enunciado : ");
+			questaoME.setEnunciado(scanner.nextLine());
+			
+			System.out.print("Insira a primeira opção : ");
+			questaoME.setOpcaoA(scanner.nextLine());
+			
+			System.out.print("Insira a segunda opção : ");
+			questaoME.setOpcaoB(scanner.nextLine());
+			
+			System.out.print("Insira a terceira opção : ");
+			questaoME.setOpcaoC(scanner.nextLine());
+		
+			boolean firstTry = true;
+		
+			while(true){
+				try{
+					System.out.print("Insira quantos pontos vale a questão : ");
+					if(firstTry == false){
+						scanner.nextLine();
+					}      
+						firstTry = false;
+					questaoME.setNotaPadrao(scanner.nextDouble());
+					scanner.nextLine();
+					break;
+				}catch(InputMismatchException ex){
+					System.out.println("Apenas números são válidos");
+				}
+			}
 
-            while(true){
-    	        try{
-    	            System.out.print("Insira a resposta correta : ");
-    	            questaoME.setOpcaoCorreta(scanner.nextLine().toUpperCase().charAt(0));
-    	            break;
-    	        }catch(NonExistentCorrectAnswer ex){
-    	            System.out.println(ex.getMessage());
-    	        }
-            }
-            System.out.println(questaoME.toString());
-	    }else{
-	        System.out.println(questaoME.toString());
-	        System.out.println("Questão já criada");
-	    }
+			while(true){
+				try{
+					System.out.print("Insira a resposta correta : ");
+					questaoME.setOpcaoCorreta(scanner.nextLine().toUpperCase().charAt(0));
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+				System.out.println(ex.getMessage());
+				}
+			}
+			System.out.println(questaoME.toString());
+		}else{
+			System.out.println(questaoME.toString());
+			System.out.println("Questão já criada");
+		}
 	}
-	
+
 	public static void criarQuestaoVF() {
-	    
-	    if(questaoVF == null){
-	        
-	        scanner.nextLine();
-	        
-	        questaoVF = new QuestaoVerdadeiroFalso();
-	        
-	        System.out.print("Insira o enunciado : ");
-	        questaoVF.setEnunciado(scanner.nextLine());
-	        
-	        System.out.print("Insira a primeira opção : ");
-	        questaoVF.setOpcaoA(scanner.nextLine());
-	        
-	        while(true){
-	            try{
-    	            System.out.print("Insira a resposta desta opção : ");
-                    questaoVF.setRespostaCorretaA(scanner.nextLine().toUpperCase().charAt(0));
-                    break;
-	            }catch(NonExistentCorrectAnswer ex){
-	                System.out.println(ex.getMessage());
-	            }
-	        } 
-	        
-	        System.out.print("Insira a segunda opção : ");
-	        questaoVF.setOpcaoB(scanner.nextLine());
-	        
-	        while(true){
-	            try{
-	                System.out.print("Insira a resposta desta opção : ");
-	                questaoVF.setRespostaCorretaB(scanner.nextLine().toUpperCase().charAt(0));
-    	            break;
-	            }catch(NonExistentCorrectAnswer ex){
-	                System.out.println(ex.getMessage());
-	            }
-            } 
-	        
-	        System.out.print("Insira a terceira opção : ");
-	        questaoVF.setOpcaoC(scanner.nextLine());
-	        
-	        while(true){
-	            try{
-    	            System.out.print("Insira a resposta desta opção : ");
-                    questaoVF.setRespostaCorretaC(scanner.nextLine().toUpperCase().charAt(0));
-                    break;
-	            }catch(NonExistentCorrectAnswer ex){
-	                System.out.println(ex.getMessage());
-	            }
-            } 
-	        
-	        boolean firstTry = true;
-	        
-            while(true){
-	            try{
-	                System.out.print("Insira quantos pontos vale a questão : ");
-	                if(firstTry == false){
-                        scanner.nextLine();
-	                }      
-	                firstTry = false;
-                    questaoVF.setNotaPadrao(scanner.nextDouble());
-                    break;
-	            }catch(InputMismatchException ex){
-	                System.out.println("Apenas números são válidos");
-	            }
-	        }
-            
-            System.out.println(questaoVF.toString());
-	    }else{
-	        System.out.println(questaoVF.toString());
-	        System.out.println("Questão já criada");
-	    }
+	
+		if(questaoVF == null){
+		
+			scanner.nextLine();
+		
+			questaoVF = new QuestaoVerdadeiroFalso();
+			
+			System.out.print("Insira o enunciado : ");
+			questaoVF.setEnunciado(scanner.nextLine());
+			
+			System.out.print("Insira a primeira opção : ");
+			questaoVF.setOpcaoA(scanner.nextLine());
+			
+			while(true){
+				try{
+					System.out.print("Insira a resposta desta opção : ");
+					questaoVF.setRespostaCorretaA(scanner.nextLine().toUpperCase().charAt(0));
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+					System.out.println(ex.getMessage());
+				}
+			} 
+		
+			System.out.print("Insira a segunda opção : ");
+			questaoVF.setOpcaoB(scanner.nextLine());
+		
+			while(true){
+				try{
+					System.out.print("Insira a resposta desta opção : ");
+					questaoVF.setRespostaCorretaB(scanner.nextLine().toUpperCase().charAt(0));
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+					System.out.println(ex.getMessage());
+				}
+			} 
+		
+			System.out.print("Insira a terceira opção : ");
+			questaoVF.setOpcaoC(scanner.nextLine());
+		
+			while(true){
+				try{
+					System.out.print("Insira a resposta desta opção : ");
+					questaoVF.setRespostaCorretaC(scanner.nextLine().toUpperCase().charAt(0));
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+					System.out.println(ex.getMessage());
+				}
+			 } 
+		
+			boolean firstTry = true;
+		
+			while(true){
+				try{
+					System.out.print("Insira quantos pontos vale a questão : ");
+					if(firstTry == false){
+						scanner.nextLine();
+					}      
+					firstTry = false;
+					questaoVF.setNotaPadrao(scanner.nextDouble());
+					break;
+				}catch(InputMismatchException ex){
+					System.out.println("Apenas números são válidos");
+				}
+			}
+		
+			System.out.println(questaoVF.toString());
+		}else{
+			System.out.println(questaoVF.toString());
+			System.out.println("Questão já criada");
+		}
 	}
 
 	public static void criarQuestaoRP() {
-	    
-	    if(questaoRP == null){
-	        
-	        scanner.nextLine();
-	        
-	        questaoRP = new QuestaoRespostaProporcional();
+	
+		if(questaoRP == null){
+		
+			scanner.nextLine();
+			
+			questaoRP = new QuestaoRespostaProporcional();
 
-            System.out.print("Insira o enunciado : ");
-            questaoRP.setEnunciado(scanner.nextLine());
-            
-            System.out.print("Insira a primeira opção : ");
-            questaoRP.setOpcaoA(scanner.nextLine());
-            
-            boolean firstTry = true;
-            
-            while(true){
-                try{
-                    System.out.print("Insira o valor desta questão : ");
-                    if(firstTry == false){
-                        scanner.nextLine();
-	                }      
-	                firstTry = false;
-                    questaoRP.setValorOpcaoA(scanner.nextInt());
-                    break;
-                }catch(NonExistentCorrectAnswer ex){
-                    System.out.println(ex.getMessage());
-                }catch(InputMismatchException ex){
-                    System.out.println("Apenas números são válidos");
-                }
-            }
-            
-            scanner.nextLine();
-            System.out.print("Insira a segunda opção : ");
-            questaoRP.setOpcaoB(scanner.nextLine());
-            
-            firstTry = true;
-            
-            while(true){
-                try{
-                    System.out.print("Insira o valor desta questão : ");
-                    if(firstTry == false){
-                        scanner.nextLine();
-	                }      
-	                firstTry = false;
-                    questaoRP.setValorOpcaoB(scanner.nextInt());
-                    break;
-                }catch(NonExistentCorrectAnswer ex){
-                    System.out.println(ex.getMessage());
-                }catch(InputMismatchException ex){
-                    System.out.println("Apenas números são válidos");
-                }
-            }
-            
-            scanner.nextLine();
-            System.out.print("Insira a terceira opção : ");
-            questaoRP.setOpcaoC(scanner.nextLine());
-            
-            firstTry = true;
-            
-            while(true){
-                try{
-                    System.out.print("Insira o valor desta questão : ");
-                    if(firstTry == false){
-                        scanner.nextLine();
-	                }      
-	                firstTry = false;
-                    questaoRP.setValorOpcaoC(scanner.nextInt());
-                    break;
-                }catch(NonExistentCorrectAnswer ex){
-                    System.out.println(ex.getMessage());
-                }catch(InputMismatchException ex){
-                    System.out.println("Apenas números são válidos");
-                }
-            }
-            
-            firstTry = true;
-            
-            while(true){
-	            try{
-	                System.out.print("Insira quantos pontos vale a questão : ");
-	                if(firstTry == false){
-                        scanner.nextLine();
-	                }      
-	                firstTry = false;
-                    questaoRP.setNotaPadrao(scanner.nextDouble());
-                    break;
-	            }catch(InputMismatchException ex){
-	                System.out.println("Apenas números são válidos");
-	            }
-	        }
-            
-            System.out.println(questaoRP.toString());
-	    }else{
-	        System.out.println(questaoRP.toString());
-	        System.out.println("Questão já criada");
-	    }
+			System.out.print("Insira o enunciado : ");
+			questaoRP.setEnunciado(scanner.nextLine());
+			
+			System.out.print("Insira a primeira opção : ");
+			questaoRP.setOpcaoA(scanner.nextLine());
+			
+			boolean firstTry = true;
+			
+			while(true){
+				try{
+					System.out.print("Insira o valor desta questão : ");
+					if(firstTry == false){
+						scanner.nextLine();
+					}      
+					firstTry = false;
+					questaoRP.setValorOpcaoA(scanner.nextInt());
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+					System.out.println(ex.getMessage());
+				}catch(InputMismatchException ex){
+					System.out.println("Apenas números são válidos");
+				}
+			}
+		
+			scanner.nextLine();
+			System.out.print("Insira a segunda opção : ");
+			questaoRP.setOpcaoB(scanner.nextLine());
+			
+			firstTry = true;
+		
+			while(true){
+				try{
+					System.out.print("Insira o valor desta questão : ");
+					if(firstTry == false){
+						scanner.nextLine();
+					}      
+					firstTry = false;
+					questaoRP.setValorOpcaoB(scanner.nextInt());
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+					System.out.println(ex.getMessage());
+				}catch(InputMismatchException ex){
+					System.out.println("Apenas números são válidos");
+				}
+			}
+		
+			scanner.nextLine();
+			System.out.print("Insira a terceira opção : ");
+			questaoRP.setOpcaoC(scanner.nextLine());
+		
+			firstTry = true;
+		
+			while(true){
+				try{
+					System.out.print("Insira o valor desta questão : ");
+					if(firstTry == false){
+						scanner.nextLine();
+					}      
+					firstTry = false;
+					questaoRP.setValorOpcaoC(scanner.nextInt());
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+					System.out.println(ex.getMessage());
+				}catch(InputMismatchException ex){
+					System.out.println("Apenas números são válidos");
+				}
+			}
+			
+			firstTry = true;
+		
+			while(true){
+				try{
+					System.out.print("Insira quantos pontos vale a questão : ");
+					if(firstTry == false){
+						scanner.nextLine();
+					}      
+					firstTry = false;
+					questaoRP.setNotaPadrao(scanner.nextDouble());
+					break;
+				}catch(InputMismatchException ex){
+					System.out.println("Apenas números são válidos");
+				}
+			}
+		
+			System.out.println(questaoRP.toString());
+		}else{
+			System.out.println(questaoRP.toString());
+			System.out.println("Questão já criada");
+		}
 	}
 	
 	public static void responderQuestaoME() {
-	    
-	    if(questaoME != null){
-	        System.out.println(questaoME.toString());
-	        scanner.nextLine();
-	        while(true){
-    	        try{
-        	        System.out.print("Resposta :  ");
-                    questaoME.marcaOpcao(scanner.nextLine().toUpperCase().charAt(0)); 
-                    break;
-    	        }catch(NonExistentCorrectAnswer ex){
-    	            System.out.println(ex.getMessage());
-    	        }
-	        }
-             System.out.println("\nNota recebida :   " + questaoME.calculaNotaObtida() + "\n");
-	    }else{
-	        System.out.println("Questão ainda não criada");
-	    }
+	
+		if(questaoME != null){
+			System.out.println(questaoME.toString());
+			scanner.nextLine();
+			while(true){
+				try{
+					System.out.print("Resposta :  ");
+					questaoME.marcaOpcao(scanner.nextLine().toUpperCase().charAt(0)); 
+					break;
+				}catch(NonExistentCorrectAnswer ex){
+					System.out.println(ex.getMessage());
+				}
+			}
+			 System.out.println("\nNota recebida :   " + questaoME.calculaNotaObtida() + "\n");
+		}else{
+			System.out.println("Questão ainda não criada");
+		}
 	}	
 
 	public static void responderQuestaoVF() {
