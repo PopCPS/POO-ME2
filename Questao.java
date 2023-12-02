7a6aa92fe00feb904e577b35c53d7cd690a52113
Artur Coelho <102534007+PopCPS@users.noreply.github.com>
2023-12-02 19:56:03 -0300

    Update Questao.java

--- Questao.java
+++ Questao.java
@@ -1,13 +1,41 @@
 public abstract class Questao implements IQuestao
 {
     protected static int contador = 1;
-    protected int idQuestao;
-    protected String enunciado;
-    protected String opcaoA;
-    protected String opcaoB;
-    protected String opcaoC;
-    protected double notaPadrao;
+    private int idQuestao;
+    private String enunciado;
+    private String opcaoA;
+    private String opcaoB;
+    private String opcaoC;
+    private double notaPadrao;
     protected double notaObtida;
+    
+    public int getIdQuestao(){
+        return this.idQuestao;
+    }
+    
+    public String getEnunciado(){
+        return this.enunciado;
+    }
+    
+    public String getOpcaoA(){
+        return this.opcaoA;
+    }
+    
+    public String getOpcaoB(){
+        return this.opcaoB;
+    }
+    
+    public String getOpcaoC(){
+        return this.opcaoC;
+    }
+    
+    public double getNotaPadrao(){
+        return this.notaPadrao;
+    }
+    
+    public void setIdQuestao(int idQuestao){
+        this.idQuestao = idQuestao;
+    }
     
     public void setEnunciado(String enunciado){
         this.enunciado = enunciado;
